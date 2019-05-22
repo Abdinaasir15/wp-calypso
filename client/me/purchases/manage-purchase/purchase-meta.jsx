0f63dc3ab5658a694cbf2ed0fe2dfb6538984071
James Tien <southp0105@gmail.com>
2019-05-22 11:38:35 +0800

    Wire the auto-renewal enabling / disabling actions to the toggle.

--- client/me/purchases/manage-purchase/purchase-meta.jsx
+++ client/me/purchases/manage-purchase/purchase-meta.jsx
@@ -28,6 +28,7 @@ import {
 	isSubscription,
 	paymentLogoType,
 } from 'lib/purchases';
+import { disableAutoRenew, enableAutoRenew } from 'lib/upgrades/actions';
 import {
 	isPlan,
 	isDomainRegistration,
@@ -37,6 +38,7 @@ import {
 import { getPlan } from 'lib/plans';
 
 import { getByPurchaseId, hasLoadedUserPurchasesFromServer } from 'state/purchases/selectors';
+import { fetchSitePurchases } from 'state/purchases/actions';
 import { getSite, isRequestingSites } from 'state/sites/selectors';
 import { getUser } from 'state/users/selectors';
 import { managePurchase } from '../paths';
@@ -301,14 +303,28 @@ class PurchaseMeta extends Component {
 	}
 
 	onToggleAutorenewal = () => {
-		// TODO: send actual autorenewal enabling / disabling action here and show a pop-up notice
-		this.setState( {
-			isAutorenewalEnabled: ! this.state.isAutorenewalEnabled,
-		} );
+		const {
+			purchase: { id: purchaseId, siteId },
+			isAutorenewalEnabled,
+		} = this.props;
+
+		if ( isAutorenewalEnabled ) {
+			disableAutoRenew( purchaseId, success => {
+				if ( success ) {
+					this.props.fetchSitePurchases( siteId );
+				}
+			} );
+		} else {
+			enableAutoRenew( purchaseId, success => {
+				if ( success ) {
+					this.props.fetchSitePurchases( siteId );
+				}
+			} );
+		}
 	};
 
 	renderExpiration() {
-		const { purchase, translate } = this.props;
+		const { purchase, translate, isAutorenewalEnabled } = this.props;
 
 		if ( isDomainTransfer( purchase ) ) {
 			return null;
@@ -322,9 +338,6 @@ class PurchaseMeta extends Component {
 			isPlan( purchase ) &&
 			! isExpired( purchase )
 		) {
-			// TODO: remove this once the proper state has been introduced.
-			const { isAutorenewalEnabled } = this.state;
-
 			const dateSpan = <span className="manage-purchase__detail-date-span" />;
 			const subsRenewText = isAutorenewalEnabled
 				? translate( 'Auto-renew is ON' )
@@ -404,14 +417,20 @@ class PurchaseMeta extends Component {
 	}
 }
 
-export default connect( ( state, { purchaseId } ) => {
-	const purchase = getByPurchaseId( state, purchaseId );
+export default connect(
+	( state, { purchaseId } ) => {
+		const purchase = getByPurchaseId( state, purchaseId );
 
-	return {
-		hasLoadedSites: ! isRequestingSites( state ),
-		hasLoadedUserPurchasesFromServer: hasLoadedUserPurchasesFromServer( state ),
-		purchase,
-		site: purchase ? getSite( state, purchase.siteId ) : null,
-		owner: purchase ? getUser( state, purchase.userId ) : null,
-	};
-} )( localize( PurchaseMeta ) );
+		return {
+			hasLoadedSites: ! isRequestingSites( state ),
+			hasLoadedUserPurchasesFromServer: hasLoadedUserPurchasesFromServer( state ),
+			purchase,
+			site: purchase ? getSite( state, purchase.siteId ) : null,
+			owner: purchase ? getUser( state, purchase.userId ) : null,
+			isAutorenewalEnabled: purchase ? ! isExpiring( purchase ) : null,
+		};
+	},
+	{
+		fetchSitePurchases,
+	}
+)( localize( PurchaseMeta ) );
